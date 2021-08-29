e89cc65fab87d36ee9a91937276deadad2f0dfb6
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-08-29 17:06:50 -0400

    Update application.py

--- application.py
+++ application.py
@@ -2,10 +2,10 @@ from flask import Flask, request, Response
 import json
 application = app = Flask(__name__)
 @app.route('/')
+def catfact():
     req = requests.get('https://catfact.ninja/fact')
     print(req.status_code)
     print(req.json())
-
     if req.status_code == 200:
         return print(f"Your fact of the day is: {req.json()['fact']}")
     else: 
