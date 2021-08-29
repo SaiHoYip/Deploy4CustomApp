c51ca4bbd00f52424701787df847c3f2df0a6a53
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-08-29 17:33:36 -0400

    Update application.py

--- application.py
+++ application.py
@@ -4,12 +4,3 @@ application = app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'hello world'
-@app.rout('/catfact')
-def catfact():
-    req = requests.get('https://catfact.ninja/fact')
-    print(req.status_code)
-    print(req.json())
-    if req.status_code == 200:
-        return "Your fact of the day is: " + req.json()['fact']
-    else: 
-        return "nope"
