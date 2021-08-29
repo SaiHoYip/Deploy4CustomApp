6899f894edaa2c433cb36e1c37b2c539a7cc1822
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-08-29 16:15:41 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
 import requests
 from PIL import Image
+from flask import Flask, request, Response
+import json
 application = app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+  
 req = requests.get('https://dog.ceo/api/breeds/image/random')
 imglink = req.json()['message']
 print(imglink)
