20e715da646b6b83e189a0e1c315c55693d81b36
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-09-06 21:39:53 -0400

    Add files via upload

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask import render_template
 import json
 import help
 import sqlite3
+
 application = app = Flask(__name__)
 
 @app.route("/", methods=["POST", "Get"])
@@ -13,15 +14,30 @@ def sub_budget():
     # Get item from the POST body
         Spent = str(request.form.get("Spent"))
         Budget = str(request.form.get("Budget"))
+        slot_id = str(request.form.get("slot_id"))
         req_data = request.get_json()
-        help.Subtraction(Budget, Spent)
+        help.Subtraction(Budget, Spent, slot_id)
+
     cat = sqlite3.connect('Budget.db')
     db = cat.cursor()
     rows = db.execute('SELECT * FROM Budgee')
     ro = rows.fetchall()
 
-        
-
     # Return response
-    return render_template("home.html", Spent = "Spent", Budget= "Budget", ro=ro)
+    return render_template("home.html", Spent = "Spent", Budget= "Budget", slot_id="slot_id", ro=ro)
 
+@app.route("/delete", methods=["POST", "GET"])
+def delete_item():
+    slot_id = 0
+    if request.method == "POST" and "slot_id" in request.form:
+        slot_id = str(request.form.get("slot_id"))
+        req_data = request.get_json()
+        help.Delete(slot_id)
+
+    cat = sqlite3.connect('Budget.db')
+    db = cat.cursor()
+    rows = db.execute('SELECT * FROM Budgee')
+    ro = rows.fetchall()
+    return render_template("home.html", ro=ro)
+
+app.run(debug=1)
--- help.py
+++ help.py
@@ -3,7 +3,7 @@ import sqlite3
 DB_PATH = "Budget.db"  # Update this path accordingly
 
 
-def Subtraction(Budget,Spent):
+def Subtraction(Budget,Spent, slot_id):
     try:
         conn = sqlite3.connect(DB_PATH)
         # Once a connection has been established, we use the cursor
@@ -11,11 +11,20 @@ def Subtraction(Budget,Spent):
         c = conn.cursor()
         # Keep the initial status as Not Started
         Left = int(Budget) - int(Spent)
-        c.execute("insert into Budgee(Budget, Spent, Left) values(?,?,?)", (Budget,Spent,str(Left),))
+        c.execute("insert into Budgee(Budget, Spent, Left, slot_id) values(?,?,?,?)", (Budget,Spent,Left,slot_id))
         # We commit to save the change
         conn.commit()
         return {"Left": Left}
     except Exception as e:
         print("Error: ", e)
         return None
+
+def Delete(slot_id):
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute('delete from Budgee where slot_id=? ', (slot_id,))
+        conn.commit()
+    except:
+        pass
 
